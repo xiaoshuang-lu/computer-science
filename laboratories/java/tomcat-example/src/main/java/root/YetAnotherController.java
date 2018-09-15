1a036d5fac61cace4401ea2f1b203acebcb484a6
Xiaoshuang LU <xiaoshuanglu01@gmail.com>
2018-09-15 19:57:53 +0800

    maven-example

---
+++ laboratories/java/tomcat-example/src/main/java/root/YetAnotherController.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2018 Xiaoshuang LU
+ *  All Rights Reserved.
+ *
+ *  Author(s):
+ *      Xiaoshuang LU <xiaoshuanglu01@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package root;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class YetAnotherController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(YetAnotherController.class);
+
+    @RequestMapping("/test/{id}")
+    @ResponseBody
+    public String test(@PathVariable("id") Long id) {
+        return "all men must die.";
+    }
+}
